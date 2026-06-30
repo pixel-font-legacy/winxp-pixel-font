af555e82a51aa66d5a18b85c62551e16d2c1f9c9
TakWolf <takwolf@foxmail.com>
2026-06-30 22:30:33 +0800

    增加 ms.bitmap.ttf、otb、dfont 构建格式

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -195,7 +195,7 @@ def dump_fonts(font_formats: list[FontFormat]) -> list[DumpLog]:
                         case 'ttf.woff2':
                             builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF2)
                         case _:
-                            getattr(builder, f'save_{font_format}')(file_path)
+                            getattr(builder, f'save_{font_format.replace('.', '_')}')(file_path)
                     logger.info("Make font: '{}'", file_path)
 
                 dump_log.font_sizes.append(builder.font_metric.font_size)
